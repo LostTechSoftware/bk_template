a911171cf561456cc48ef671c77aff03a0eca64d
João Augusto <joao.augusto@foodzilla.com.br>
2021-08-28 20:54:40 -0300

    feat: configuring aws setup

--- src/scripts/index.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  defaultScriptTemplate: require('./defaultScriptTemplate'),
-  createUsersHubSpot: require('./createUsersHubSpot'),
-  createUserSendPulse: require('./createUserSendPulse'),
-  labelProducts: require('./labelProducts'),
-  removingOldPrices: require('./removingOldPrices'),
-}
